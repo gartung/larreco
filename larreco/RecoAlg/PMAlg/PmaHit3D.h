a7f4d7c1e5c30606bedaac1e12f1aabd38f437b3
Kyle Knoepfel <knoepfel@fnal.gov>
2019-09-05 10:00:53 -0500

    Clang-format only.

--- larreco/RecoAlg/PMAlg/PmaHit3D.h
+++ larreco/RecoAlg/PMAlg/PmaHit3D.h
@@ -5,8 +5,8 @@
  *
  *  @brief  Implementation of the Projection Matching Algorithm
  *
- *          Hit 3D wrapped around recob::Hit. Adds support for PMA optimizations.
- *          See PmaTrack3D.h file for details.
+ *          Hit 3D wrapped around recob::Hit. Adds support for PMA
+ * optimizations. See PmaTrack3D.h file for details.
  */
 
 #ifndef PmaHit3D_h
@@ -20,86 +20,182 @@
 #include "TVector2.h"
 #include "TVector3.h"
 
-namespace pma
-{
-	class Hit3D;
-	class Track3D;
+namespace pma {
+  class Hit3D;
+  class Track3D;
 }
 
-class pma::Hit3D
-{
-	friend class Track3D;
-	friend struct bTrajectory3DOrderLess;
+class pma::Hit3D {
+  friend class Track3D;
+  friend struct bTrajectory3DOrderLess;
 
 public:
-	Hit3D();
-	Hit3D(art::Ptr< recob::Hit > src);
-	Hit3D(unsigned int wire, unsigned int view, unsigned int tpc, unsigned int cryo,
-		float peaktime, float ampl, float area);
-	Hit3D(const pma::Hit3D& src);
-	virtual ~Hit3D() {}
+  Hit3D();
+  Hit3D(art::Ptr<recob::Hit> src);
+  Hit3D(unsigned int wire,
+        unsigned int view,
+        unsigned int tpc,
+        unsigned int cryo,
+        float peaktime,
+        float ampl,
+        float area);
+  Hit3D(const pma::Hit3D& src);
+  virtual ~Hit3D() {}
 
-	art::Ptr< recob::Hit > const & Hit2DPtr() const { return fHit; }
+  art::Ptr<recob::Hit> const&
+  Hit2DPtr() const
+  {
+    return fHit;
+  }
 
-	TVector3 const & Point3D() const { return fPoint3D; }
+  TVector3 const&
+  Point3D() const
+  {
+    return fPoint3D;
+  }
 
-	void SetPoint3D(const TVector3& p3d) { fPoint3D = p3d; }
-	void SetPoint3D(double x, double y, double z) { fPoint3D.SetXYZ(x, y, z); }
+  void
+  SetPoint3D(const TVector3& p3d)
+  {
+    fPoint3D = p3d;
+  }
+  void
+  SetPoint3D(double x, double y, double z)
+  {
+    fPoint3D.SetXYZ(x, y, z);
+  }
 
-	TVector2 const & Point2D() const { return fPoint2D; }
-	TVector2 const & Projection2D() const { return fProjection2D; }
+  TVector2 const&
+  Point2D() const
+  {
+    return fPoint2D;
+  }
+  TVector2 const&
+  Projection2D() const
+  {
+    return fProjection2D;
+  }
 
-	unsigned int Cryo() const { return fCryo; }
-	unsigned int TPC() const { return fTPC; }
-	unsigned int View2D() const { return fPlane; }
-	unsigned int Wire() const { return fWire; }
-	float PeakTime() const { return fPeakTime; }
+  unsigned int
+  Cryo() const
+  {
+    return fCryo;
+  }
+  unsigned int
+  TPC() const
+  {
+    return fTPC;
+  }
+  unsigned int
+  View2D() const
+  {
+    return fPlane;
+  }
+  unsigned int
+  Wire() const
+  {
+    return fWire;
+  }
+  float
+  PeakTime() const
+  {
+    return fPeakTime;
+  }
 
-	float SummedADC() const { return fArea; }
-	float GetAmplitude() const { return fAmpl; }
-	float GetSigmaFactor() const { return fSigmaFactor; }
-	void SetSigmaFactor(float value) { fSigmaFactor = value; }
+  float
+  SummedADC() const
+  {
+    return fArea;
+  }
+  float
+  GetAmplitude() const
+  {
+    return fAmpl;
+  }
+  float
+  GetSigmaFactor() const
+  {
+    return fSigmaFactor;
+  }
+  void
+  SetSigmaFactor(float value)
+  {
+    fSigmaFactor = value;
+  }
 
-	double Dx() const { return fDx; }
+  double
+  Dx() const
+  {
+    return fDx;
+  }
 
-	double GetDistToProj() const { return sqrt(GetDist2ToProj()); }
-	double GetDist2ToProj() const;
+  double
+  GetDistToProj() const
+  {
+    return sqrt(GetDist2ToProj());
+  }
+  double GetDist2ToProj() const;
 
-	float GetSegFraction() const { return fSegFraction; }
-	void SetProjection(const TVector2& p, float b)
-	{
-		fProjection2D.Set(p); fSegFraction = b;
-	}
-	void SetProjection(double x, double y, float b)
-	{
-		fProjection2D.Set(x, y); fSegFraction = b;
-	}
+  float
+  GetSegFraction() const
+  {
+    return fSegFraction;
+  }
+  void
+  SetProjection(const TVector2& p, float b)
+  {
+    fProjection2D.Set(p);
+    fSegFraction = b;
+  }
+  void
+  SetProjection(double x, double y, float b)
+  {
+    fProjection2D.Set(x, y);
+    fSegFraction = b;
+  }
 
-	bool IsEnabled() const { return (fEnabled && !fOutlier); }
-	void SetEnabled(bool state) { fEnabled = state; }
+  bool
+  IsEnabled() const
+  {
+    return (fEnabled && !fOutlier);
+  }
+  void
+  SetEnabled(bool state)
+  {
+    fEnabled = state;
+  }
 
-	bool IsOutlier() const { return fOutlier; }
-	virtual void TagOutlier(bool state) { fOutlier = state; }
+  bool
+  IsOutlier() const
+  {
+    return fOutlier;
+  }
+  virtual void
+  TagOutlier(bool state)
+  {
+    fOutlier = state;
+  }
 
 private:
+  art::Ptr<recob::Hit> fHit; // source 2D hit
 
-	art::Ptr< recob::Hit > fHit;  // source 2D hit
+  unsigned int fCryo, fTPC, fPlane, fWire;
+  float fPeakTime, fAmpl, fArea;
 
-	unsigned int fCryo, fTPC, fPlane, fWire;
-	float fPeakTime, fAmpl, fArea;
+  TVector3 fPoint3D;      // hit position in 3D space
+  TVector2 fPoint2D;      // hit position in 2D wire view, scaled to [cm]
+  TVector2 fProjection2D; // projection to polygonal line in 2D wire view,
+                          // scaled to [cm]
+  float fSegFraction;     // segment fraction set by the projection
+  float fSigmaFactor;     // impact factor on the objective function
 
-	TVector3 fPoint3D;       // hit position in 3D space
-	TVector2 fPoint2D;       // hit position in 2D wire view, scaled to [cm]
-	TVector2 fProjection2D;  // projection to polygonal line in 2D wire view, scaled to [cm]
-	float fSegFraction;      // segment fraction set by the projection
-	float fSigmaFactor;      // impact factor on the objective function
+  double fDx; // dx seen by corresponding 2D hit, set during dQ/dx sequece
+              // calculation
 
-	double fDx;    // dx seen by corresponding 2D hit, set during dQ/dx sequece calculation
+  bool fEnabled; // used or not in the optimisation - due to various reasons
+  bool fOutlier; // tagged as a not really hit of this track (like delta ray)
 
-	bool fEnabled; // used or not in the optimisation - due to various reasons
-	bool fOutlier; // tagged as a not really hit of this track (like delta ray)
-
-	pma::Track3D* fParent;   // track which contains this hit
+  pma::Track3D* fParent; // track which contains this hit
 };
 
 #endif
